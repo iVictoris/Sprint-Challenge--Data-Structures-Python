6f5725bb0a020af7254572a42128acb0ad8e664b
Victor <victor.tran@tempestas.io>
2020-07-16 16:27:20 -0400

    use BST for duplicate finder, reduce time from 8s to .3s

--- names/names.py
+++ names/names.py
@@ -1,22 +1,31 @@
 import time
 
+from bst import BSTNode
+
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
+bst_1 = BSTNode(names_1)
 f.close()
 
 f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
+bst_2 = BSTNode(names_2)
 f.close()
 
-duplicates = []  # Return the list of duplicates in this data structure
+# bst_1.in_order_print()
 
-# Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+duplicates = bst_1.find_duplicates(bst_2) #first_list.get_duplicates(second_list)  # Return the list of duplicates in this data structure
+# Replace the nested for loops below with your improvements # this is O(n^2)
+# The only way to make this a log n or even o(1) (hash map) is to be able to divide a list kinda sort of like a binary tree
+# for name_1 in names_1:
+#     for name_2 in names_2:
+# # 
+
+
+# so only return the duplicates
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
