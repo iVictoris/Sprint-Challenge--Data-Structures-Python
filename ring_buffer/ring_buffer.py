5ac0f4944199a7d34b348bc712484398bd70fb7d
Victor <victor.tran@tempestas.io>
2020-07-16 15:00:32 -0400

    start sprint at 3pm EST 7/16/2020

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -7,3 +7,26 @@ class RingBuffer:
 
     def get(self):
         pass
+
+# start sprint at 3pm EST 7/16/2020
+"""
+buffer = RingBuffer(3)
+
+buffer.get()   # should return []
+
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+
+buffer.get()   # should return ['a', 'b', 'c']
+
+# 'd' overwrites the oldest value in the ring buffer, which is 'a'
+buffer.append('d')
+
+buffer.get()   # should return ['d', 'b', 'c']
+
+buffer.append('e')
+buffer.append('f')
+
+buffer.get()   # should return ['d', 'e', 'f']
+"""
